6e5e27b3bcc6381d35644fa74f8af2ecdecf0d46
atekasu <ataranaize@gmail.com>
2025-06-25 21:43:22 +0900

    空のファイルを削除しました。

--- lib/screen/machine_list_screen.dart
+++ lib/screen/machine_list_screen.dart
@@ -1,7 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:farmflow/data/machine_dummy_data.dart';
-import 'package:farmflow/widget/machine_card.dart';
-import 'package:farmflow/widget/machine_status_filter.dart';
 
 ///機会一覧画面（トラクター管理画面）
 class MachineListScreen extends StatefulWidget {
@@ -14,12 +11,8 @@ class _MachineListScreenState extends State<MachineListScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('機会一覧'),
-      ),
-      body: const Center(
-        child: Text('機会一覧画面の内容をここに追加してください'),
-      ),
+      appBar: AppBar(title: const Text('機会一覧')),
+      body: const Center(child: Text('機会一覧画面の内容をここに追加してください')),
     );
   }
 }
